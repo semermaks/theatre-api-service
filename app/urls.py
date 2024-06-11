8d28a0b70479bb2f8db49247156ce5a686b5df39
semermaks <semermaks@gmail.com>
2024-06-11 11:13:47 +0300

    Fix urls

--- app/urls.py
+++ app/urls.py
@@ -26,7 +26,7 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/cinema/", include("theatre.urls", namespace="theatre")),
+    path("api/theatre/", include("theatre.urls", namespace="theatre")),
     path("api/user/", include("user.urls", namespace="user")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
