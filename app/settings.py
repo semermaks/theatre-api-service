d7ecb995898a4a481a106046aa7f11a83d632128
semermaks <semermaks@gmail.com>
2024-06-12 11:34:59 +0300

    Secret key to .env

--- app/settings.py
+++ app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4l-lv&6=iyvc%06@oqf8&iuh!uq*90zjx87rqv6xg&t-67uct#'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
